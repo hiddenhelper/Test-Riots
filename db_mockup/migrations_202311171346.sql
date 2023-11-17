4a614c0a04a7aa8b8a61857f0dfd748f4a8e7ac4
hiddenhelper <bountyhunter0308@hotmail.com>
2023-11-17 13:02:36 -0600

    complete test

---
+++ db_mockup/migrations_202311171346.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.migrations ("timestamp",name) VALUES
+	 (1700156087331,'Migration1700156087331');
---
+++ db_mockup/players_202311171346.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.players (summoner_name,summoner_region,win,kda,kills,cs_per_minuts,average_vision_score,assits,league_points,"createdAt") VALUES
+	 ('TTV LegitKorea','NA1',1,1,1,1,1,1,1,'2023-11-16 23:36:53.278259'),
+	 ('TTV LegitKorea','NA1',1,1,1,1,1,1,1,'2023-11-16 23:37:24.768881'),
+	 ('TTV LegitKorea','NA1',1,1,1,1,1,1,1,'2023-11-16 23:38:48.044025');
